a09a5c649ca7b4482242d27ad5701d153fb3d269
INCONNU-MD <ha9384761@gmail.com>
2024-08-14 20:56:50 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,84 @@
+//INCONNU TECH
+const fs = require("fs-extra");
+if (fs.existsSync(".env"))
+  require("dotenv").config({ path: __dirname + "/.env" });
+
+//=======[dependencies]====================//
+global.SESSION_ID = process.env.SESSION_ID || "";
+global.MONGODB = process.env.MONGODB_URI || "";
+global.DATABASE_URL = process.env.DATABASE_URL || "";
+global.sudo = process.env.SUDO
+  ? process.env.SUDO.replace(/[\s+]/g, "")
+  : "null";
+global.owner = process.env.OWNER_NUMBER
+  ? process.env.OWNER_NUMBER.replace(/[\s+]/g, "")
+  : "null";
+global.THUMB_IMAGE =
+  process.env.THUMB_IMAGE ||
+  process.env.IMAGE ||
+  "![IMG-20240813-WA0406](https://github.com/user-attachments/assets/32af4af6-2fe8-42bd-be2e-f06cbc755f66)";
+global.userImages =
+  process.env.USER_IMAGES ||
+  "https://telegra.ph/file/a7112fe9501c0e3e44dae.jpg,https://telegra.ph/file/1496d1ee606e8946930bd.jpg";
+///===========[global iMPORTS]====================//
+
+module.exports = {
+  menu: process.env.MENU || "",
+  HANDLERS: process.env.PREFIX || "",
+  BRANCH: process.env.BRANCH || "main",
+  VERSION: process.env.VERSION || "1.0.0",
+  caption: process.env.CAPTION || "`©LAUREAT-MD`",
+  author: process.env.PACK_AUTHER || "LAUREAT-MD",
+  packname: process.env.PACK_NAME || "INCONNU-TECH",
+  botname: process.env.BOT_NAME || "LAUREAT-MD",
+  ownername: process.env.OWNER_NAME || "INCONNU-TECH",
+  errorChat: process.env.ERROR_CHAT || "",
+  KOYEB_API: process.env.KOYEB_API || "false",
+  REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
+  antilink_values: process.env.ANTILINK_VALUES || "all",
+  HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
+  aitts_Voice_Id: process.env.AITTS_ID || "37",
+  ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
+  LANG: (process.env.THEME || "WhatsApp").toUpperCase(),
+};
+global.port = process.env.PORT;
+global.appUrl = process.env.APP_URL || "";
+global.email = "";
+global.location = "";
+global.allowJids = process.env.ALLOW_JID || "null";
+global.blockJids = process.env.BLOCK_JID || "null";
+global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Douala";
+global.github = process.env.GITHUB || "https://github.com/Inconnuboy34/Lone-tomioka-";
+global.gurl = process.env.GURL || "";
+global.website = process.env.GURL || "";
+global.devs = "50936698203";
+global.msg_style = process.env.STYLE || "4";
+global.session_reset = process.env.SS_RESET || "false";
+global.gdbye = process.env.GOODBYE || "false";
+global.wlcm = process.env.WELCOME || "false";
+global.warncount = process.env.WARN_COUNT || 3;
+global.disablepm = process.env.DISABLE_PM || "false";
+(global.disablegroup = process.env.DISABLE_GROUPS || "false"),
+  (global.MsgsInLog = process.env.MSGS_IN_LOG || "true");
+global.waPresence = process.env.WAPRESENCE || "online";
+global.readcmds = process.env.READ_COMMAND || "false";
+global.readmessage = process.env.READ_MESSAGE || "false";
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null";
+global.read_status = process.env.AUTO_READ_STATUS || "false";
+global.save_status = process.env.AUTO_SAVE_STATUS || "false";
+global.save_status_from = process.env.SAVE_STATUS_FROM || "null";
+global.read_status_from = process.env.READ_STATUS_FROM || "null";
+global.api_smd = "https://api-smd-1.vercel.app";
+global.scan = "https://laureat-session.onrender.com";
+global.isMongodb = false;
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+  fs.unwatchFile(file);
+  console.log(`Update'${__filename}'`);
+  delete require.cache[file];
+  require(file);
+});
